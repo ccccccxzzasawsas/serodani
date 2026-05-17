6d950f4c56cbf582668cc4a1dc884154f0c38289
leqso7 <1osqela@gmail.com>
2026-05-17 20:53:02 +0400

    s

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <footer className="py-4 bg-[#242323] border-t border-gray-700">
       <div className="container mx-auto px-4">
         <div className="text-center text-gray-400 text-sm">
-          <p>© 2025 SERODANI Hotel | All Rights Reserved</p>
+          <p>© 2026 SERODANI Hotel | All Rights Reserved</p>
         </div>
       </div>
     </footer>
